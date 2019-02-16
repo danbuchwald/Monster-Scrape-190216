53b11267bd692e5a7c847863382b81e8808b19bc
James Gary <gmarchesi888@gmail.com>
2019-02-16 18:41:24 -0500

    Server listens on port, connects to MongoDB.

--- server.js
+++ server.js
@@ -3,8 +3,26 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
+
 const port = process.env.PORT;
 const dataFromDB = require("./response.json");
 
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true })
+  .then(
+    () => console.log("Connected to the database"),
+    err => console.log("Cannot connect to database " + err)
+  );
+
 console.log("First entry:", dataFromDB.results[0].url);
-console.log("Port is: ", port);
+// console.log("Connnection string: ", process.env.DB);
+
+app.get("/", (req, res) => {
+  res.send("Hello world!!!!");
+});
+
+app.listen(port, () => {
+  console.log("Server listening on port", port);
+});
