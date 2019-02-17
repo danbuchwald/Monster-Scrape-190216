c08fc6697c687b0e9f6621f4a4f72178432c64e9
James Gary <gmarchesi888@gmail.com>
2019-02-17 11:46:14 -0500

    Gets a single entry from D&D API.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
+const axios = require("axios");
 
 const port = process.env.PORT;
 const dataFromDB = require("./response.json");
@@ -16,9 +17,14 @@ mongoose
     err => console.log("Cannot connect to database " + err)
   );
 
-console.log("First entry:", dataFromDB.results[0].url);
-// console.log("Connnection string: ", process.env.DB);
+const currentEntry = dataFromDB.results[0].url;
+console.log("First entry:", currentEntry);
 
+axios
+  .get(currentEntry)
+  .then(response => console.log(response.data))
+  .catch(err => console.log(err));
+  
 app.get("/", (req, res) => {
   res.send("Hello world!!!!");
 });
